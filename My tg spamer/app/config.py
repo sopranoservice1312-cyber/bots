4b52d2ee7bc4fcc3edf68038958c130ec4c7e397
sopranoservice1312-cyber <sopranoservice1312@gmail.com>
2025-08-22 10:09:25 +0300

    Update config.py

--- My tg spamer/app/config.py
+++ My tg spamer/app/config.py
@@ -2,8 +2,8 @@ import os
 
 class Settings:
     # Можно задать свои значения по умолчанию или получать из env
-    TELEGRAM_API_ID = int(os.getenv("TG_API_ID", "123456"))
-    TELEGRAM_API_HASH = os.getenv("TG_API_HASH", "your_default_hash")
+    TELEGRAM_API_ID = int(os.getenv("TG_API_ID", "29626149"))
+    TELEGRAM_API_HASH = os.getenv("TG_API_HASH", "c95627c11e62cba634e03fff562494e2")
     SCHEDULER_TICK_SECONDS = 5
 
 settings = Settings()
