4c54426ba651db7e84138b43c77352cee9cf65de
sopranoservice1312-cyber <sopranoservice1312@gmail.com>
2025-09-10 17:40:01 +0300

    Update models.py

--- My tg spamer/app/models.py
+++ My tg spamer/app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Text, Boolean, DateTime, ForeignKey, JSON
+from sqlalchemy import Column, Integer, String, Text, Boolean, DateTime, BigInteger, ForeignKey, JSON
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 from .database import Base
